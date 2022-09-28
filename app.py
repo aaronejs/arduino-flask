a5b377155abcdff07046e5b5b4a4aefcd402cdf9
Aaron <56259354+aaronejs@users.noreply.github.com>
2022-09-28 19:09:32 +0200

    Update app.py
    Added functions to calculate minimum and maximum

--- app.py
+++ app.py
@@ -29,19 +29,40 @@ def LDRMeasure(data):
     global light
     light = data[2]
 
+def CalcMax(type : str):
+    max = 0
+
+    for i in statistics:
+        for key, value in i.items():
+            if type == key:
+                if value > max:
+                    max = value
+
+    return max
+
 def CalcAverage(type : str):
     counter = 0
     avg = 0
     for i in statistics:
-        if i != statistics[0]:
-            for key, value in i.items():
-                if type == key:
-                    avg += value
-                    counter += 1
+        for key, value in i.items():
+            if type == key:
+                avg += value
+                counter += 1
 
     if counter < 1:
         return 0.0
     return avg/counter
+
+def CalcMin(type : str):
+    min = 10000
+    for i in statistics:
+        for key, value in i.items():
+            if type == key:
+                if value < min:
+                    min = value
+    
+    return min
+
 
 @app.route("/")
 def main():
@@ -53,9 +74,9 @@ def main():
     if len(statistics) < rows+1:
         statistics.append({ "time" : now, "temperature" : temperature, "humidity" : humidity, "light" : light})
     
-        statistics_calc[0] = {"max_temp" : 0, "avg_temp" : round(CalcAverage("temperature"), 2), "min_temp" : 0}
-        statistics_calc[1] = {"max_humidity" : 0, "avg_humidity" : round(CalcAverage("humidity"), 2), "min_humidity" : 0}
-        statistics_calc[2] = {"max_light" : 0, "avg_light" : round(CalcAverage("light"), 2), "min_light" : 0}
+        statistics_calc[0] = {"max_temp" : round(CalcMax("temperature"), 2), "avg_temp" : round(CalcAverage("temperature"), 2), "min_temp" : round(CalcMin("temperature"), 2)}
+        statistics_calc[1] = {"max_humidity" : round(CalcMax("humidity"), 2), "avg_humidity" : round(CalcAverage("humidity"), 2), "min_humidity" : round(CalcMin("humidity"), 2)}
+        statistics_calc[2] = {"max_light" : round(CalcMax("light"), 2), "avg_light" : round(CalcAverage("light"), 2), "min_light" : round(CalcMin("light"), 2)}
         
         if len(statistics) > rows:
             del statistics[0]
