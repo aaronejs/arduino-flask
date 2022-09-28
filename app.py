14f1fe682fb36a0ed7206a25b7684d10c6b8910e
Aaron <56259354+aaronejs@users.noreply.github.com>
2022-09-28 19:02:34 +0200

    Separated calculated data from raw data list

--- app.py
+++ app.py
@@ -3,10 +3,18 @@ from flask import Flask
 from flask import render_template
 from datetime import datetime as dt
 from fhict_cb_01.CustomPymata4 import CustomPymata4
+import time
 
 LDRPIN = 2
 DHTPIN = 12
-statistics = [{"sensor_id" : 0, "average_temperature" : 0.0, "average_humidity" : 0.0, "average_light" : 0}]
+light = humidity = temperature = 0
+statistics = []
+statistics_calc = [
+    {"max_temp" : 0, "avg_temp" : 0, "min_temp" : 0},
+    {"max_humidity" : 0, "avg_humidity" : 0, "min_humidity" : 0},
+    {"max_light" : 0, "avg_light" : 0, "min_light" : 0}
+]
+
 app = Flask(__name__)
 board = CustomPymata4(com_port = "COM8")
 
@@ -31,17 +39,26 @@ def CalcAverage(type : str):
                     avg += value
                     counter += 1
 
+    if counter < 1:
+        return 0.0
     return avg/counter
 
 @app.route("/")
 def main():
+    rows = 10
     now = dt.now().strftime('%H:%M:%S')
     board.set_pin_mode_analog_input(LDRPIN, differential = 10, callback = LDRMeasure)
     board.set_pin_mode_dht(DHTPIN, sensor_type=11, differential=.05, callback = Measure)
 
-    if len(statistics) < 15:
+    if len(statistics) < rows+1:
         statistics.append({ "time" : now, "temperature" : temperature, "humidity" : humidity, "light" : light})
-        statistics[0] = {"sensor_id" : 0, "average_temperature" : round(CalcAverage("temperature"), 2), 
-                        "average_humidity" : round(CalcAverage("humidity"), 2), "average_light" : round(CalcAverage("light"), 2)} 
     
-    return render_template('time.html', statistics = statistics)
+        statistics_calc[0] = {"max_temp" : 0, "avg_temp" : round(CalcAverage("temperature"), 2), "min_temp" : 0}
+        statistics_calc[1] = {"max_humidity" : 0, "avg_humidity" : round(CalcAverage("humidity"), 2), "min_humidity" : 0}
+        statistics_calc[2] = {"max_light" : 0, "avg_light" : round(CalcAverage("light"), 2), "min_light" : 0}
+        
+        if len(statistics) > rows:
+            del statistics[0]
+    
+
+    return render_template('time.html', statistics = statistics, statistics_calc = statistics_calc)
