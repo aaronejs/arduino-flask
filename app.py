e7377b06df79007edd5d47e691eba02fb8b9cde1
Aaron <56259354+aaronejs@users.noreply.github.com>
2022-09-28 19:29:00 +0200

    Update app.py

--- app.py
+++ app.py
@@ -63,23 +63,22 @@ def CalcMin(type : str):
     
     return min
 
-
 @app.route("/")
 def main():
-    rows = 10
+    rows = 3
     now = dt.now().strftime('%H:%M:%S')
     board.set_pin_mode_analog_input(LDRPIN, differential = 10, callback = LDRMeasure)
     board.set_pin_mode_dht(DHTPIN, sensor_type=11, differential=.05, callback = Measure)
 
     if len(statistics) < rows+1:
-        statistics.append({ "time" : now, "temperature" : temperature, "humidity" : humidity, "light" : light})
-    
-        statistics_calc[0] = {"max_temp" : round(CalcMax("temperature"), 2), "avg_temp" : round(CalcAverage("temperature"), 2), "min_temp" : round(CalcMin("temperature"), 2)}
-        statistics_calc[1] = {"max_humidity" : round(CalcMax("humidity"), 2), "avg_humidity" : round(CalcAverage("humidity"), 2), "min_humidity" : round(CalcMin("humidity"), 2)}
-        statistics_calc[2] = {"max_light" : round(CalcMax("light"), 2), "avg_light" : round(CalcAverage("light"), 2), "min_light" : round(CalcMin("light"), 2)}
+        if temperature > 0 and humidity > 0 and light > 0:
+
+            statistics.append({ "time" : now, "temperature" : temperature, "humidity" : humidity, "light" : light})
         
-        if len(statistics) > rows:
-            del statistics[0]
-    
+            statistics_calc[0] = {"max_temp" : round(CalcMax("temperature"), 2), "avg_temp" : round(CalcAverage("temperature"), 2), "min_temp" : round(CalcMin("temperature"), 2)}
+            statistics_calc[1] = {"max_humidity" : round(CalcMax("humidity"), 2), "avg_humidity" : round(CalcAverage("humidity"), 2), "min_humidity" : round(CalcMin("humidity"), 2)}
+            statistics_calc[2] = {"max_light" : round(CalcMax("light"), 2), "avg_light" : round(CalcAverage("light"), 2), "min_light" : round(CalcMin("light"), 2)}
+            if len(statistics) > rows:
+                del statistics[0]
 
     return render_template('time.html', statistics = statistics, statistics_calc = statistics_calc)
