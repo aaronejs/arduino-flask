1a9b3a2a5417635c187fad6137a92af0499c36c8
Aaron <56259354+aaronejs@users.noreply.github.com>
2022-10-18 09:44:17 +0200

    Fixes according to feedback
    removed recursion because sometimes it throws RecursionError. Added a sleep instead to wait for sensor to pick a value
    
    Changed loop to be more readable

--- client.py
+++ client.py
@@ -3,12 +3,14 @@ from fhict_cb_01.CustomPymata4 import CustomPymata4
 import requests
 import random as r
 import csv
+import time as t
 
 fieldnames = ['sensor_id', 'time', 'temperature','humidity','light']
 board = CustomPymata4(com_port = "COM8")
 DHTPIN = 12
 LDRPIN = 2
 temperature = humidity = light = 0
+expectingInpt = True
 
 def setup():
     board.set_pin_mode_dht(DHTPIN, sensor_type=11, differential=.05, callback = Measure)
@@ -30,16 +32,16 @@ def getCurTime():
 
 def writeCSV(maxLines : int):
     if temperature == 0:
-        writeCSV(maxLines)
-    else:
-        data = {'sensor_id': 1337, 'time':getCurTime(), 'temperature':temperature, 'humidity':humidity, 'light':light}
+        t.sleep(2)
 
-        with open('data.csv') as f:
-            rows = sum(1 for line in f)
-        if rows < maxLines+1:
-            with open('data.csv', 'a', newline='') as csvfile:
-                writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-                writer.writerow(data)
+    data = {'sensor_id': 1337, 'time':getCurTime(), 'temperature':temperature, 'humidity':humidity, 'light':light}
+
+    with open('data.csv') as f:
+        rows = sum(1 for line in f)
+    if rows < maxLines+1:
+        with open('data.csv', 'a', newline='') as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            writer.writerow(data)
 
     return data
 
@@ -62,7 +64,7 @@ def getAvg(url : str):
     print("Average received from server: " + str(temp) + " degrees")
 
 setup()
-while True:
+while expectingInpt:
     url = input("Please enter the IP address of the Flask server: ")
     inpt = input('S-send data, G-get average, Q-quit\n')
 
@@ -76,5 +78,7 @@ while True:
         sendData(url)
     elif inpt in 'gG':
         getAvg(url)
-    else:
+    elif inpt in 'qQ':
         break
+    else:
+        print('\nWrong input!\n')
